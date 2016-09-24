9fb3e887e620ae641a95f8f554aaef6e855e55b0
Muthukrishnan <muthu@vidyartha.com>
2016-09-24 20:06:30 +0530

    fixed grunt dist folder path

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: '/var/www/html/squirrelbox'
+    dist: 'dist'
   };
 
   // Define the configuration for all the tasks
